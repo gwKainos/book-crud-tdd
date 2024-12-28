e0f8f271f919a49df842326d8fe1d3e6b820805b
Kainos <gwkainos@gmail.com>
2024-12-28 23:25:58 +0900

    feat: 리포지토리 생성

---
+++ src/main/java/kr/kainos/book/book/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package kr.kainos.book.book.repository;
+
+import kr.kainos.book.book.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
